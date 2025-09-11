69540848d0acb8b129e5d2a8aeffc1b9b6b17d90
xavierscript <david111francisco@gmail.com>
2025-09-11 23:51:06 +0100

    finished footer

--- aces-website/src/components/footer.tsx
+++ aces-website/src/components/footer.tsx
@@ -194,7 +194,7 @@ export default function Footer() {
               </div>
               <div>
                 <p className="text-gray-300">Address: ACES Secretariat,</p>
-                <p className="text-gray-300">Faculty of Engineering, UNiben</p>
+                <p className="text-gray-300">Faculty of Engineering, UNIben</p>
               </div>
             </div>
           </div>
